87116a3322d197ceb3775439ad4bc9f5e8605501
exotol <artem.bardakov@gmail.com>
2021-12-07 01:26:54 +0300

    Добавил скрипт для загрузки модели и прописал скачивание при установке

---
+++ scripts/download.sh
@@ -0,0 +1,3 @@
+python -m spacy download en_core_web_sm
+wget https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin
+gdown --id 1-1Usk7sM1aydyZFEyTetaY7tydwoG1dC
--- setup.py
+++ setup.py
@@ -8,13 +8,7 @@ def req_file(filename, folder=""):
     with open(os.path.join(folder, filename)) as f:
         content = f.readlines()
     return [x.strip() for x in content]
-
-
   
-  
-if os.path.exists("scripts/download.sh"):
-    print(subprocess.run(["sh", "scripts/download.sh"], capture_output=True))
-	
 setup(
     name="feature_transformer_made_marusya",
     packages=find_packages(),
@@ -25,3 +19,8 @@ setup(
     install_requires=req_file("requirements.txt"),
     license="MIT",
 )
+
+
+if os.path.exists("scripts/download.sh"):
+    print(subprocess.run(["sh", "scripts/download.sh"], capture_output=True))
+	
