c71a7df0268aa9aef371556e9e4c1d283f7a7562
exotol <artem.bardakov@gmail.com>
2021-12-06 23:40:01 +0300

    добавил опция как "экстра" требование при устновке

--- setup.py
+++ setup.py
@@ -6,11 +6,16 @@ def req_file(filename, folder=""):
         content = f.readlines()
     return [x.strip() for x in content]
 
+extras = {
+    "all": req_file("requirements-nemo.txt")
+}
+	
 setup(
     name="feature_transformer_made_marusya",
     packages=find_packages(),
     version="0.1.0",
     description="For graduation project in MADE academy",
+	extras_require=extras,
     author="arhimisha",
     install_requires=req_file("requirements.txt"),
     license="MIT",
