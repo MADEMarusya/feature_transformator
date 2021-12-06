868ecedf762651a11c54a0e04c3ccbb8c413b0a7
exotol <artem.bardakov@gmail.com>
2021-12-07 00:50:31 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(),
     version="0.1.0",
     description="For graduation project in MADE academy",
-	extras_require=extras,
+    #extras_require=extras,
     author="arhimisha",
     install_requires=req_file("requirements.txt"),
     license="MIT",
